4d0398b417acbea60e3f1dc80deb85912530b02d
Moritz <moritz.j.egger@gmail.com>
2015-10-12 14:45:27 +0200

    set defaults settings

--- js/provider/settings.js
+++ js/provider/settings.js
@@ -4,6 +4,18 @@
 app.factory('settingsFactory',function($q){
     var config = require('./conf/config.json');
 
+    function initDefaultConfig(){
+        var defaults;
+        defaults.language = "en";
+        defaults.anime_stream = "torrent";
+        defaults.style.name = "skin-red";
+        defaults.style.path = "./plugins/AdminLTE/dist/css/skins/skin-red.min.css";
+
+        logger.info("Default Settings init", defaults);
+
+        config = defaults;
+    }
+
     return{
         getConfigJSON:function(){
             return JSON.stringify(config);
